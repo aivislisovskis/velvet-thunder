eb9ae54441af7e1cbfb1d1dee5bb9acee2721c4c
Arturs Zigurs <arturs.zigurs@avanade.com>
2017-11-25 01:03:19 +0200

    Added JsonDataService and sample data in factories.json

--- Backend/VelvetThunderApiMF/Controllers/HomeController.cs
+++ Backend/VelvetThunderApiMF/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VelvetThunderApiMF.Services;
 
 namespace VelvetThunderApiMF.Controllers
 {
@@ -11,7 +12,8 @@ namespace VelvetThunderApiMF.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            JsonDataService jsonDataService = new JsonDataService();
+            jsonDataService.readJson();
             return View();
         }
     }
