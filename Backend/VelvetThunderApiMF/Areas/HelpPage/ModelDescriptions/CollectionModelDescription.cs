cebb98785344aaae6e1b904f10bad9a590f7fe63
Arturs Zigurs <arturs.zigurs@avanade.com>
2017-11-24 23:33:10 +0200

    Initial VS solution.

---
+++ Backend/VelvetThunderApiMF/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VelvetThunderApiMF.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
