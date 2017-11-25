df5107fdd40228237009b530c6bedb69a35280b9
Arturs Zigurs <arturs.zigurs@avanade.com>
2017-11-25 22:48:48 +0200

    Added HttpPost endpoint for adding product.

--- Backend/VelvetThunderApiMF/Services/RamaniApiClient.cs
+++ Backend/VelvetThunderApiMF/Services/RamaniApiClient.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using Newtonsoft.Json;
 
@@ -37,12 +44,15 @@ namespace VelvetThunderApiMF.Services
             //string Adress = @"https://ramani.ujuizi.com/ddl/wms?item=layerDetails&layerName=simS5seriesNO2Europe/no2_conc&time=2010-11-05T00:00:00.000Z&request=GetMetadata&token=e247bab10844647700de5f317e5f9611&package=com.zigurs93.ramaniapi";
             string requestAdress = "https://ramani.ujuizi.com/cloud/wms/ramaniddl/tilecache?SERVICE=WMS&REQUEST=GetMap&VERSION=1.1.1&LAYERS=ddl.simS1seriesTwenteNetherlands.smc&STYLES=boxfill%2Frainbow&FORMAT=image%2Fpng&TRANSPARENT=true&HEIGHT=256&WIDTH=256&STYLE=boxfill%2Frainbow&SRS=EPSG%3A3857&ZINDEX=400&REUSETILES=true&TOKEN=&PACKAGE=&COLORSCALERANGE=&ELEVATION=0&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&LOGSCALE=false&NUMCOLORBANDS=250&TIME=2010-11-29T00%3A00%3A00.000Z&BBOX=665307.8941941741,6887893.492833804,704443.6526761843,6927029.251315814&token=e247bab10844647700de5f317e5f9611&package=com.zigurs93.ramaniapi";
 
-            HttpClient httpClient = new HttpClient();
-            return  httpClient.GetStringAsync(requestAdress).Result;
-            //dynamic getMetadataObject = JsonConvert.DeserializeObject(response);
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tocken);
+            Stream stream = client.GetStreamAsync(requestAdress).Result;
 
-            //string units = getMetadataObject["units"];
-            //string nearestTimeIso = getMetadataObject["nearestTimeIso"];`
+
+            //HttpClient httpClient = new HttpClient();
+            //return  httpClient.GetStringAsync(requestAdress).Result;
+
         }
     }
 }
