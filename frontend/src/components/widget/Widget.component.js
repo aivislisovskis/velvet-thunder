6932db7d265f3bbb92e73eadc4ff6cd5a0e0128c
ilze-a <ilze.dombrovska@accenture.com>
2017-11-25 14:12:19 +0200

    badge

--- frontend/src/components/widget/Widget.component.js
+++ frontend/src/components/widget/Widget.component.js
@@ -11,9 +11,40 @@ class WidgetComponent extends React.Component {
   render() {
     const { props } = this;
     return (
-     <div>
-       Grra
-     </div>
+      <div className={style.vt-logo}>
+      <div className={style.upper}>
+        <span className={style.char1}>s</span>
+        <span className={style.char2}>u</span>
+        <span className={style.char3}>s</span>
+        <span className={style.char4}>t</span>
+        <span className={style.char5}>a</span>
+        <span className={style.char6}>i</span>
+        <span className={style.char7}>n</span>
+        <span className={style.char8}>a</span>
+        <span className={style.char9}>b</span>
+        <span className={style.char10}>i</span>
+        <span className={style.char11}>l</span>
+        <span className={style.char12}>i</span>
+        <span className={style.char13}>t</span>
+        <span className={style.char14}>y</span>
+      </div>
+      <div className={style.star}>
+        <div className={style.seal}>
+          <div className={style.face}>
+            <div className={style.evaluation}>
+            {props.score}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={style.lower}>
+        <span className={style.char1}>s</span>
+        <span className={style.char2}>c</span>
+        <span className={style.char3}>o</span>
+        <span className={style.char4}>r</span>
+        <span className={style.char5}>e</span>
+      </div>
+    </div>
     );
   }
 }
