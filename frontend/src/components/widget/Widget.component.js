edc669cd8be9897a2d9d1f68b64d7f0ee56db9e8
ilze-a <ilze.dombrovska@accenture.com>
2017-11-25 14:24:44 +0200

    fix badge

--- frontend/src/components/widget/Widget.component.js
+++ frontend/src/components/widget/Widget.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import style from './Widget.css';
+import './Widget.css';
 
 class WidgetComponent extends React.Component {
   constructor(props) {
@@ -11,38 +11,38 @@ class WidgetComponent extends React.Component {
   render() {
     const { props } = this;
     return (
-      <div className={style.vt-logo}>
-      <div className={style.upper}>
-        <span className={style.char1}>s</span>
-        <span className={style.char2}>u</span>
-        <span className={style.char3}>s</span>
-        <span className={style.char4}>t</span>
-        <span className={style.char5}>a</span>
-        <span className={style.char6}>i</span>
-        <span className={style.char7}>n</span>
-        <span className={style.char8}>a</span>
-        <span className={style.char9}>b</span>
-        <span className={style.char10}>i</span>
-        <span className={style.char11}>l</span>
-        <span className={style.char12}>i</span>
-        <span className={style.char13}>t</span>
-        <span className={style.char14}>y</span>
+      <div className="vtlogo">
+      <div className="upper">
+        <span className="char1">s</span>
+        <span className="char2">u</span>
+        <span className="char3">s</span>
+        <span className="char4">t</span>
+        <span className="char5">a</span>
+        <span className="char6">i</span>
+        <span className="char7">n</span>
+        <span className="char8">a</span>
+        <span className="char9">b</span>
+        <span className="char10">i</span>
+        <span className="char11">l</span>
+        <span className="char12">i</span>
+        <span className="char13">t</span>
+        <span className="char14">y</span>
       </div>
-      <div className={style.star}>
-        <div className={style.seal}>
-          <div className={style.face}>
-            <div className={style.evaluation}>
+      <div className="star">
+        <div className="seal">
+          <div className="face">
+            <div className="evaluation">
             {props.score}
             </div>
           </div>
         </div>
       </div>
-      <div className={style.lower}>
-        <span className={style.char1}>s</span>
-        <span className={style.char2}>c</span>
-        <span className={style.char3}>o</span>
-        <span className={style.char4}>r</span>
-        <span className={style.char5}>e</span>
+      <div className="lower">
+        <span className="char1">s</span>
+        <span className="char2">c</span>
+        <span className="char3">o</span>
+        <span className="char4">r</span>
+        <span className="char5">e</span>
       </div>
     </div>
     );
