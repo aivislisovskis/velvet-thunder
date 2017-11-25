e46957276e86556e338d64a9fdb3eec07665a263
Aivis <aivis@aivis.lv>
2017-11-25 17:08:55 +0200

    Added request

--- frontend/src/components/widget/Widget.component.js
+++ frontend/src/components/widget/Widget.component.js
@@ -5,14 +5,16 @@ import style from './Widget.css';
 class WidgetComponent extends React.Component {
   constructor(props) {
     super(props);
-    props.getScore();
+    props.getScore(props.match.params.productId);
   }
 
   render() {
     const { props } = this;
     return (
      <div>
+       <span className={style.colored}>
        Grra
+     </span>
      </div>
     );
   }
